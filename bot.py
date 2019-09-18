a5f2330f0bcea566ff44b031d8e9e08cee4f59cb
AmateurGamer94 <55400655+AmateurGamer94@users.noreply.github.com>
2019-09-19 00:06:26 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,27 +1,22 @@
 import discord
 import datetime
 
-token = 'NjE5Mzc4NTk0NTE0NDY4ODY0.XXJqHw.ar9RgG5r59SgxrsUr9-YSiWFsic'
+token = 'NjE5Mzc4NTk0NTE0NDY4ODY0.XYD0bQ.nYdN1P5mD9uRS3-PW8jRpKfPL_Y'
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+        print('Log in completed')
 
     async def on_message(self, message):
         if message.author.id == self.user.id:
             return
         if message.content.startswith('!'):
             now = datetime.datetime.now()
-            for i in range(3):
-                embed = discord.Embed(title="이거슨 제목이라 합니다!", description="sadfasfasdfdasfd\nasdfasfasdfadsf\nasdfafsd", color=0x888888)
-                embed.set_thumbnail(url='D:/Discord/profiles/dokkaebi.webp')
-                embed.set_image(url='https://i.imgur.com/xzPCXp8.jpg')
-                embed.set_author(name='hehe')
-                if i == 2: embed.set_footer(text = '%s년 %s월 %s일 %s %s:%s'%(now.year, now.month, now.day, '오전' if now.hour < 13 else '오후', now.hour if now.hour < 13 else now.hour - 12, now.minute))
-                await message.channel.send(embed=embed)
+            embed = discord.Embed(title="이거슨 제목이라 합니다!", description="sadfasfasdfdasfd\nasdfasfasdfadsf\nasdfafsd", color=0x888888)
+            embed.set_thumbnail(url='https://github.com/AmateurGamer94/r6s_bot/blob/master/profiles/amaru.webp')
+            embed.set_image(url='https://i.imgur.com/xzPCXp8.jpg')
+            embed.set_footer(text = '%s년 %s월 %s일 %s %s:%s'%(now.year, now.month, now.day, '오전' if now.hour < 13 else '오후', now.hour if now.hour < 13 else now.hour - 12, now.minute))
+            await message.channel.send(embed=embed)
 
 client = MyClient()
 client.run(token)
--- control.py
+++ control.py
@@ -1,15 +1,9 @@
 import codecs
 
-profileText = '**%s**\n[아이콘 사진]\n```[프로필 사진]\n이름:%s\n소속:%s\n생일:%s\n나이:%s\n출생지:%s\n\n역할:%s\n방어구_%s\n속도_%s```'
+profileText = '**%s %s**\n[아이콘 사진]\n```[프로필 사진]\n이름:%s\n소속:%s\n생일:%s\n나이:%s\n출생지:%s\n\n역할:%s\n방어구_%s\n속도_%s```'
 f = codecs.open('database.txt', 'r', 'utf-8').readlines()
 lines = []
 data = {}
-
-def a(i):
-    return '■' * i + '□' * (3 - i)
-
-def b(i):
-    return '■' * (4 - i) + '□' * (i - 1)
 
 for i in range(len(f)):
     f[i] = f[i].replace('\n', '')
@@ -17,6 +11,11 @@ for i in range(len(f)):
     if(f[i][1] == '^'):
         f[i][1] = f[i-1][1]
     else:
-        array = f[i][1].split(';')
-        f[i][1] = profileText%(array[0],array[1],array[2],array[3],array[4],array[5],array[6],array[7],array[8])
+        f[i][1] = f[i][1].split(';')
+        print(f[i][1])
+        j = int(f[i][1][7])
+        x = '■' * j + '□' * (3 - j)
+        y = '■' * (4 - j) + '□' * (i - j)
+        f[i][1] = profileText%(f[i][0].capitalize(), f[i][1][0], f[i][1][1], f[i][1][2], f[i][1][3], f[i][1][4], f[i][1][5], f[i][1][6], x, y)
     data[f[i][0]] = f[i][1]
+    data[f[i][1][0]] = f[i][1]
