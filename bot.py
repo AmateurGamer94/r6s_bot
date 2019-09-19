78c2206c636e6fc444be4ae21a8b8c4e4d9c9252
AmateurGamer94 <55400655+AmateurGamer94@users.noreply.github.com>
2019-09-20 08:04:53 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,18 @@
 import discord
+import codecs
 import datetime
 
-token = 'NjE5Mzc4NTk0NTE0NDY4ODY0.XYD0bQ.nYdN1P5mD9uRS3-PW8jRpKfPL_Y'
+token = 'NjE5Mzc4NTk0NTE0NDY4ODY0.XYNLZQ.PznhhekhIAX73z3FQMgumZM7os8'
+f = codecs.open('database.txt', 'r', 'utf-8').readlines()
+color = 0x888888
+commands = {}
+
+for i in range(len(f)):
+    f[i] = f[i].split()
+    f[i][1] = f[i][1].split(';')
+    f[i][1].insert(0,f[i][0].capitalize())
+    commands[f[i][0]] = f[i][1]
+    commands[f[i][1][1]] = f[i][1]
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -10,11 +21,17 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.author.id == self.user.id:
             return
-        if message.content.startswith('!'):
+        if message.content.startswith('!') and commands in message.content[1:]:
+            embed = discord.Embed(title='%s %s', description='나이\n생일\n방어구', color=color)
+            embed.set_thumbnail(url='https://raw.githubusercontent.com/AmateurGamer94/r6s_bot/master/profiles/amaru.webp')
+            embed.set_image(url='https://raw.githubusercontent.com/AmateurGamer94/r6s_bot/master/portrait/amaru.webp')
+            embed = discord.Embed(title='Amaru 아마루', description='나이\n생일\n방어구', color=color)
+            embed.set_thumbnail(url='https://raw.githubusercontent.com/AmateurGamer94/r6s_bot/master/profiles/amaru.webp')
+            embed.set_image(url='https://raw.githubusercontent.com/AmateurGamer94/r6s_bot/master/portrait/amaru.webp')
+            embed = discord.Embed(title='Amaru 아마루', description='나이\n생일\n방어구', color=color)
+            embed.set_thumbnail(url='https://raw.githubusercontent.com/AmateurGamer94/r6s_bot/master/profiles/amaru.webp')
+            embed.set_image(url='https://raw.githubusercontent.com/AmateurGamer94/r6s_bot/master/portrait/amaru.webp')
             now = datetime.datetime.now()
-            embed = discord.Embed(title="이거슨 제목이라 합니다!", description="sadfasfasdfdasfd\nasdfasfasdfadsf\nasdfafsd", color=0x888888)
-            embed.set_thumbnail(url='https://github.com/AmateurGamer94/r6s_bot/blob/master/profiles/amaru.webp')
-            embed.set_image(url='https://i.imgur.com/xzPCXp8.jpg')
             embed.set_footer(text = '%s년 %s월 %s일 %s %s:%s'%(now.year, now.month, now.day, '오전' if now.hour < 13 else '오후', now.hour if now.hour < 13 else now.hour - 12, now.minute))
             await message.channel.send(embed=embed)
 
